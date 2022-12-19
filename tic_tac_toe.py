0b0173f9ec3fe52165376f9f0e903a6076e68494
Evgeniy Korsakov <evgeniy.korsakov@hostwaycorp.com>
2022-12-19 22:04:00 +0600

    updated a little after webinar.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -2,14 +2,14 @@
 CONTROL_LIST = (0, 1, 2)
 
 
-def draw_field():
+def draw_field(f):
     print(f"  0 1 2")
-    print(f"0 {field[0 + 3 * 0]} {field[1 + 3 * 0]} {field[2 + 3 * 0]}")
-    print(f"1 {field[0 + 3 * 1]} {field[1 + 3 * 1]} {field[2 + 3 * 1]}")
-    print(f"2 {field[0 + 3 * 2]} {field[1 + 3 * 2]} {field[2 + 3 * 2]}")
+    print(f"0 {f[0 + 3 * 0]} {f[1 + 3 * 0]} {f[2 + 3 * 0]}")
+    print(f"1 {f[0 + 3 * 1]} {f[1 + 3 * 1]} {f[2 + 3 * 1]}")
+    print(f"2 {f[0 + 3 * 2]} {f[1 + 3 * 2]} {f[2 + 3 * 2]}")
 
 
-def do_move(a):
+def do_move(f, a):
     while True:
         ask_move = input(f"Please enter coordinate for {a}, where format for X=1 and Y=2 should be like 12: ")
         if all([ask_move.isdigit(),
@@ -17,11 +17,10 @@ def do_move(a):
                 int(ask_move[0]) in CONTROL_LIST,
                 int(ask_move[1]) in CONTROL_LIST]):
             coordinate = int(ask_move[0]) + 3 * int(ask_move[1])
-            if field[coordinate] == '-':
-                field[coordinate] = a
-                break
+            if f[coordinate] == '-':
+                return coordinate
             else:
-                print(f"That move already done by {field[coordinate]}, please choose another move.")
+                print(f"That move already done by {f[coordinate]}, please choose another move.")
                 continue
         else:
             print(f"Coordinates should be in format like 12. "
@@ -29,15 +28,15 @@ def do_move(a):
             continue
 
 
-def check_winner(a):
-    if any([field[0] == field[1] == field[2] == a,
-            field[3] == field[4] == field[5] == a,
-            field[6] == field[7] == field[8] == a,
-            field[0] == field[3] == field[6] == a,
-            field[1] == field[4] == field[7] == a,
-            field[2] == field[5] == field[8] == a,
-            field[0] == field[4] == field[8] == a,
-            field[2] == field[4] == field[6] == a]):
+def check_winner(f, a):
+    if any([f[0] == f[1] == f[2] == a,
+            f[3] == f[4] == f[5] == a,
+            f[6] == f[7] == f[8] == a,
+            f[0] == f[3] == f[6] == a,
+            f[1] == f[4] == f[7] == a,
+            f[2] == f[5] == f[8] == a,
+            f[0] == f[4] == f[8] == a,
+            f[2] == f[4] == f[6] == a]):
         return True
     else:
         return False
@@ -47,13 +46,14 @@ print("Welcome to tic tac toe game. New round.")
 count_rounds = 1
 while True:
     field = ['-' for i in range(9)]
-    print(draw_field())
+    print(draw_field(field))
     for i in range(1, 10):
         print(f"There is {i} move.")
-        current_player = 'Y' if i % 2 else 'X'
-        do_move(current_player)
-        print(draw_field())
-        if i > 3 and check_winner(current_player):
+        current_player = 'X' if i % 2 else 'Y'
+        cord = do_move(field, current_player)
+        field[cord] = current_player
+        print(draw_field(field))
+        if i > 3 and check_winner(field, current_player):
             print(f"Winner is player {current_player}!")
             break
         elif i == 9:
